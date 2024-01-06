fc744a4f6f18d9f661664931ae917ed1e4c66e7b
samehmahfooz <samehmahfooz@localhost>
2024-01-05 19:05:36 -0500

    Printed "Pushing to github"

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -5,5 +5,8 @@ public class TestDemo {
 	public static void main(String[] args) {
 		
 		System.out.println("Java is my life");
+		
+		//push to github
+		System.out.println("Pushing to github");
 	}
 }
