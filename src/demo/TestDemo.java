91c292c2b32757503fa20fd8e6558fdf589927a3
samehmahfooz <samehmahfooz@localhost>
2024-01-05 20:34:53 -0500

    Printed "GitHub"

--- src/demo/TestDemo.java
+++ src/demo/TestDemo.java
@@ -4,10 +4,14 @@ public class TestDemo {
 
 	public static void main(String[] args) {
 		
+		System.out.println("GitHub");
+		
 		System.out.println("Java is my life");
 		
 		//push to github
 		System.out.println("Pushing to github");
+		
+		System.out.println("Today is not tuesday");
 	}
 	//today is friday
 }
